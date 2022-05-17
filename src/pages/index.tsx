0fcc4a0c1ccb478e31dabc13563f2f1f2333eab1
Marnoux <marnouxmanser@gmail.com>
2022-05-17 16:25:49 +0200

    Add app skeleton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import Layout from '../components/Layout/Layout';
 
 const Home: NextPage = () => {
 	return (
-		<div>
-			<h1>mBudget</h1>
-		</div>
+		<>
+			<Layout />
+		</>
 	);
 };
 
