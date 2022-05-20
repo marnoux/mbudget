2316da1f6245446a2d0e1636b674f2bbd9402791
Marnoux <marnouxmanser@gmail.com>
2022-05-20 16:25:09 +0200

    Replace Firebase config with Supabase config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	env: {
-		REACT_APP_FIREBASE_API_KEY: 'AIzaSyBoRFKmft37reeekxty0aaE0r1J3ySMoj4',
-		REACT_APP_FIREBASE_AUTH_DOMAIN: 'react-http-5f20c.firebaseapp.com',
-		REACT_APP_FIREBASE_DATABASE_URL:
-			'https://react-http-5f20c-default-rtdb.europe-west1.firebasedatabase.app',
-		REACT_APP_FIREBASE_PROJECT_ID: 'react-http-5f20c',
-		REACT_APP_FIREBASE_STORAGE_BUCKET: 'react-http-5f20c.appspot.com',
-		REACT_APP_FIREBASE_MESSAGING_SENDER_ID: '577155931419',
-		REACT_APP_FIREBASE_APP_ID: '1:577155931419:web:52bb8badacb98b708887be',
+		REACT_APP_SUPABASE_DATABASE_URL: 'https://uajdvxanfbszwvtitcfo.supabase.co',
 	},
 	compiler: {
 		styledComponents: true,
